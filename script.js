2770067f20c2d375d333dec6f8732ec3064a95b7
Peter-Parras <102561250+Peter-Parras@users.noreply.github.com>
2023-03-08 14:31:08 -0500

    Addition of dynamic city search

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+    "apiKey": "3de42674123682b8e7a2324cde6f8559",
+    fetchWeather: function (lat, lon) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?lat="
+            + lat
+            + "&lon="
+            + lon 
+            + "&units=imperial&appid="
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed" + speed + "mph";
+    }
+};
