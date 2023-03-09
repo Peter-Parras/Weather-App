252c34398e3b860f534eca327f28e9a05026f030
Peter-Parras <102561250+Peter-Parras@users.noreply.github.com>
2023-03-08 20:43:10 -0500

    Search bar function

--- script.js
+++ script.js
@@ -24,5 +24,18 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed" + speed + "mph";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+weather.fetchWeather("41.49, -81.69")
